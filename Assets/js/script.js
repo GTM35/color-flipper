13934a9c4cab6732a747bb0ab074d37f77fe2c32
Gustavo Teofilo <035guuh@gmail.com>
2022-07-07 13:45:13 -0300

    Arrumando o botão de click para o mobile pt2

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -55,4 +55,4 @@ async function Options() {
 }
 
 document.querySelector("#button").addEventListener("click", Options);
-/* document.querySelector("#button").addEventListener("touchend", Options); */
+document.querySelector("#button").addEventListener("touchend", Options);
